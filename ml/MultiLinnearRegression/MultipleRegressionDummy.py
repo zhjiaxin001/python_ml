43e986b950843c85b9b1c94ace081001a37329a2
e <l>
2018-11-14 00:38:03 +0800

    多元线性回归实现

---
+++ ml/MultiLinnearRegression/MultipleRegressionDummy.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+from numpy import genfromtxt
+import numpy as np
+from sklearn import datasets, linear_model
+datePath = r"Delivery_Dummy.csv"
+deliverData = genfromtxt(datePath, delimiter=',')
+print 'data'
+print deliverData
+
+X = deliverData[:, : -1]
+Y = deliverData[:, -1]
+
+print 'X:'
+print X
+
+print 'Y:'
+print Y
+
+abc = linear_model.LinearRegression()
+abc.fit(X, Y)
+
+print('coefficients:')
+print abc.coef_
+print 'intercept:'
+print abc.intercept_
+xPred = [102, 6]
+yPred = abc.predict(xPred)
+print 'y:'
+print yPred
