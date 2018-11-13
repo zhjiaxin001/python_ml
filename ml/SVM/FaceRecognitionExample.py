dfe9a50d618730c309b7d0538bd2ab8190cb28f2
e <l>
2018-11-13 22:18:15 +0800

    SLR demo

--- ml/SVM/FaceRecognitionExample.py
+++ ml/SVM/FaceRecognitionExample.py
@@ -48,3 +48,49 @@ t0 = time()
 x_train_pca = pca.transform(x_train)
 x_test_pca = pca.transform(x_test)
 print("done in %0.3fs"%(time()-t0))
+
+print("Fitting the classifier to the training set")
+t0 = time()
+param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+              'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1]}# 尝试不同的值,，C:对于错误的部分进行惩罚,gamma:
+clf = GridSearchCV(SVC(kernel='rbf'),param_grid)
+clf = clf.fit(x_train_pca,y_train)
+print("done in %0.3fs"%(time()-t0))
+print("Best estimator found by grid search:")
+print(clf.best_estimator_)
+
+#
+print("predicting peoples's banes ib tge test set")
+t0 = time()
+y_pred = clf.predict(x_test_pca)
+print("done in %0.3fs"%(time()-t0))
+
+print(classification_report(y_test, y_pred, target_names=target_names)) # 预估精确率分析
+print(confusion_matrix(y_test, y_pred, labels=range(n_classes)))  # 对角线的值相对越大，越准确
+
+def  plot_gallery(images, titles, h, w, n_row =3, n_col=4):
+    plt.figure(figsize=(1.8*n_col, 2.4*n_row))
+    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.99, hspace=.35)
+    for i in range(n_row*n_col):
+        plt.subplot(n_row, n_col, i+1)
+        plt.imshow(images[i].reshape((h, w)),cmap=plt.cm.gray)
+        plt.title(titles[i], size=12)
+        plt.xticks(())
+        plt.yticks(())
+
+# plot the result of the prediction on a portion of the test set
+
+def title(y_pred, y_test, target_names,i):
+    pred_name = target_names[y_pred[i]].rsplit(' ',1 )[-1]
+    true_name = target_names[y_test[i]].rsplit(' ',1 )[-1]
+    return "predicted:%s\ntrue ;    %s"% (pred_name,true_name)
+prediction_titles = [title(y_pred, y_test, target_names, i)
+                     for i in range(y_pred.shape[0])]
+plot_gallery(x_test, prediction_titles, h, w)
+
+# plot the gallery of the most significative eigenfaces
+eigenfaces_titles = ["eigenface %d"% i for i in range(eigenfaces.shape[0])]
+plot_gallery(eigenfaces, eigenfaces_titles, h, w)
+plt.show()
+
+
